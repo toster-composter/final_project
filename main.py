a6da3f40deebd868866643899689fe48161cba7c
toster-composter <89584574+toster-composter@users.noreply.github.com>
2023-10-29 21:32:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import sqlite3
 
-# Основной класс приложения
+# Main application class
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
@@ -10,27 +10,33 @@ class Main(tk.Frame):
         self.db = DB()
         self.view_record()
 
-    # Инициализация главного окна
+    # Initialize the main window and UI components
     def init_main(self):
+        # Create a toolbar
         tool_bar = tk.Frame(bg='#d7d8e0', bd=2)
         tool_bar.pack(side=tk.TOP, fill=tk.X)
 
-        self.tree = ttk.Treeview(self, columns=('ID', 'name', 'number', 'email'), height=45, show='headings')
+        # Create a Treeview widget for displaying records
+        self.tree = ttk.Treeview(self, columns=('ID', 'name', 'number', 'email', 'salary'), height=45, show='headings')
         scroll = tk.Scrollbar(self, command=self.tree.yview)
         scroll.pack(side=tk.RIGHT, fill=tk.Y)
         self.tree.config(yscrollcommand=scroll.set)
 
+        # Define columns and headings for the Treeview
         self.tree.column('ID', width=30, anchor=tk.CENTER)
         self.tree.column('name', width=300, anchor=tk.CENTER)
         self.tree.column('number', width=150, anchor=tk.CENTER)
         self.tree.column('email', width=150, anchor=tk.CENTER)
+        self.tree.column('salary', width=200, anchor=tk.CENTER)
 
         self.tree.heading('ID', text='ID')
         self.tree.heading('name', text='ФИО')
-        self.tree.heading('number', text='tel. number')
+        self.tree.heading('number', text='Номер тел.')
         self.tree.heading('email', text='E-mail')
+        self.tree.heading('salary', text='Зарплата')
         self.tree.pack(side=tk.LEFT)
 
+        # Create buttons for various operations
         self.add_img = tk.PhotoImage(file='img/add.png')
         btn_open_dialog = tk.Button(tool_bar, bg='#d7d8e0', bd=0, image=self.add_img, command=self.open_dialog)
         btn_open_dialog.pack(side=tk.LEFT)
@@ -47,75 +53,78 @@ class Main(tk.Frame):
         btn_search = tk.Button(tool_bar, bg='#d7d8e0', bd=0, image=self.search_img, command=self.open_search)
         btn_search.pack(side=tk.LEFT)
 
-        self.refresh_img = tk.PhotoImage(file='img/refresh.png')
+        self.refresh_img = tk.PhotoImage(file='test_project/img/refresh.png')
         btn_refresh = tk.Button(tool_bar, bg='#d7d8e0', bd=0, image=self.refresh_img, command=self.view_record)
         btn_refresh.pack(side=tk.LEFT)
 
-    # Открывает диалог для добавления записи
+    # Open a dialog to add a new record
     def open_dialog(self):
         NewWindow()
 
-    # Записывает данные в базу данных и обновляет отображение
-    def record(self, name, tel, email):
-        self.db.insert_data(name, tel, email)
+    # Record data to the database and refresh the view
+    def record(self, name, tel, email, salary):
+        self.db.insert_data(name, tel, email, salary)
         self.view_record()
 
-    # Отображает все записи из базы данных в таблице
+    # Display all records from the database in the table
     def view_record(self):
         self.db.cur.execute('SELECT * FROM db')
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
 
-    # Открывает диалог для редактирования записи
+    # Open a dialog to edit a record
     def open_editor(self):
         Update()
 
-    # Обновляет запись в базе данных и обновляет отображение
-    def update_record(self, name, tel, email):
-        self.db.cur.execute('''UPDATE db SET name = ?, tel = ?, email = ? WHERE id = ?''',
-                            (name, tel, email, self.tree.set(self.tree.selection()[0], '#1')))
+    # Update a record in the database and refresh the view
+    def update_record(self, name, tel, email, salary):
+        self.db.cur.execute('''UPDATE db SET name = ?, tel = ?, email = ?, salary = ? WHERE id = ?''',
+                            (name, tel, email, salary, self.tree.set(self.tree.selection()[0], '#1')))
         self.db.conn.commit()
         self.view_record()
 
-    # Удаляет запись из базы данных и обновляет отображение
+    # Delete a record from the database and refresh the view
     def delete_record(self):
         for select_item in self.tree.selection():
             self.db.cur.execute('DELETE FROM db WHERE id = ?', self.tree.set(select_item, '#1'))
         self.db.conn.commit()
         self.view_record()
 
-    # Открывает диалог для поиска записи
+    # Open a dialog to search for a record
     def open_search(self):
         Search()
 
-    # Выполняет поиск по записям и обновляет отображение
-    def search_contact(self, name):
+    # Search for records and refresh the view
+    def search_employee(self, name):
         name = ('%' + name + '%')
         self.db.cur.execute('SELECT * FROM db WHERE name LIKE ?', (name,))
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
 
-
-# Диалог для добавления записи
+# Dialog for adding a record
 class NewWindow(tk.Toplevel):
     def __init__(self):
         super().__init__(root)
         self.init_new_window()
         self.view = app
 
+    # Initialize the dialog for adding a new record
     def init_new_window(self):
-        self.title('ADD')
+        self.title('Добавить сотрудника')
         self.geometry('400x200')
         self.resizable(False, False)
         self.grab_set()
         self.focus_set()
 
+        # Labels and entry fields for user input
         label_name = tk.Label(self, text='ФИО')
         label_name.place(x=50, y=50)
-        label_select = tk.Label(self, text='tel. number')
+        label_select = tk.Label(self, text='Номер тел.')
         label_select.place(x=50, y=80)
         label_email = tk.Label(self, text='E-mail')
         label_email.place(x=50, y=110)
+        label_salary = tk.Label(self, text='Зарплата')
+        label_salary.place(x=50, y=140)
 
         self.entry_name = ttk.Entry(self)
         self.entry_name.place(x=200, y=50)
@@ -123,20 +132,23 @@ class NewWindow(tk.Toplevel):
         self.entry_number.place(x=200, y=80)
         self.entry_email = ttk.Entry(self)
         self.entry_email.place(x=200, y=110)
+        self.entry_salary = ttk.Entry(self)
+        self.entry_salary.place(x=200, y=140)
 
-        self.btn_cancel = ttk.Button(self, text='close', command=self.destroy)
+        # Buttons for adding and closing the dialog
+        self.btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
         self.btn_cancel.place(x=300, y=170)
 
-        self.btn_ok = ttk.Button(self, text='add')
+        self.btn_ok = ttk.Button(self, text='Добавить')
         self.btn_ok.place(x=220, y=170)
         self.btn_ok.bind('<Button-1>', lambda event:
                          self.view.record(self.entry_name.get(),
                          self.entry_number.get(),
-                         self.entry_email.get()))
+                         self.entry_email.get(),
+                         self.entry_salary.get()))
         self.btn_ok.bind('<Button-1>', lambda event: self.destroy(), add='+')
 
-
-# Диалог для редактирования записи
+# Dialog for editing a record
 class Update(NewWindow):
     def __init__(self):
         super().__init__()
@@ -144,72 +156,77 @@ class Update(NewWindow):
         self.view = app
         self.db = db
 
+    # Initialize the dialog for updating a record
     def init_update(self):
-        self.title('Edit contact')
+        self.title('Изменить сотрудника')
 
-        btn_edit = ttk.Button(self, text='Edit')
+        btn_edit = ttk.Button(self, text='Изменить')
         btn_edit.place(x=205, y=170)
         btn_edit.bind('<Button-1>', lambda event:
                       self.view.update_record(self.entry_name.get(),
                       self.entry_number.get(),
-                      self.entry_email.get()))
+                      self.entry_email.get(),
+                      self.entry_salary.get()))
         btn_edit.bind('<Button-1>', lambda event: self.destroy(), add='+')
         self.btn_ok.destroy()
 
+    # Populate the fields with the data of the selected record
     def default_data(self):
         self.db.cur.execute('SELECT * FROM db WHERE id = ?', (self.view.tree.set(self.view.tree.selection()[0], '#1')))
         row = self.db.cur.fetchall()
         self.entry_name.insert(0, row[1])
         self.entry_number.insert(0, row[2])
         self.entry_email.insert(0, row[3])
+        self.entry_salary.insert(0, row[4])
 
-
-# Диалог для поиска записи
+# Dialog for searching for a record
 class Search(tk.Toplevel):
     def __init__(self):
         super().__init__()
         self.init_search()
         self.view = app
 
+    # Initialize the search dialog
     def init_search(self):
-        self.title('Search for contact')
+        self.title('Искать сотрудника')
         self.geometry('300x100')
         self.resizable(False, False)
 
-        label_search = tk.Label(self, text='Name')
+        label_search = tk.Label(self, text='Имя')
         label_search.place(x=50, y=20)
 
         self.entry_search = ttk.Entry(self)
         self.entry_search.place(x=105, y=20, width=150)
 
-        btn_cancel = ttk.Button(self, text='close', command=self.destroy)
+        btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
         btn_cancel.place(x=185, y=50)
 
-        btn_search = ttk.Button(self, text='Search')
+        btn_search = ttk.Button(self, text='Искать')
         btn_search.place(x=105, y=50)
         btn_search.bind('<Button-1>', lambda event:
-                         self.view.search_contact(self.entry_search.get()))
+                         self.view.search_employee(self.entry_search.get()))
         btn_search.bind('<Button-1>', lambda event:
                          self.destroy(), add='+')
 
-
-# Класс для работы с базой данных
+# Database class
 class DB:
     def __init__(self):
-        self.conn = sqlite3.connect('test_db.db')
+        self.conn = sqlite3.connect('MAIN_db.db')
         self.cur = self.conn.cursor()
         self.table = self.cur.execute('''
         CREATE TABLE IF NOT EXISTS db (
             id INTEGER PRIMARY KEY,
             name TEXT,
             tel TEXT,
-            email TEXT
+            email TEXT,
+            salary TEXT
             );
         ''')
         self.conn.commit()
 
-    def insert_data(self, name, tel, email):
-        self.cur.execute('INSERT INTO db (name, tel, email) VALUES (?, ?, ?);', (name, tel, email))
+    # Insert data into the database
+    def insert_data(self, name, tel, email, salary):
+        self.cur.execute('INSERT INTO db (name, tel, email, salary) VALUES (?, ?, ?, ?);', (name, tel, email, salary))
         self.conn.commit()
 
 if __name__ == '__main__':
@@ -218,7 +235,7 @@ if __name__ == '__main__':
     app = Main(root)
     app.pack()
 
-    root.title('number book')
-    root.geometry('650x450')
+    root.title('Список сотрудников компании')
+    root.geometry('840x450')
     root.resizable(False, False)
     root.mainloop()
